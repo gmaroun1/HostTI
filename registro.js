d66106a36f1e7e2ad9e2c39483dfcfdf5f0b2b8f
gmaroun1 <gabrielbmaroun@gmail.com>
2024-06-06 19:19:45 -0300

    .

--- registro.js
+++ registro.js
@@ -45,7 +45,7 @@ function incluirCadastro () {
             objDados.cadastros.push (novoCadastro);
             // Salvar novamente
             salvaDados (objDados);
-            window.location.assign('http://127.0.0.1:5500/login.html')
+            window.location.assign('/login.html')
         }
     }
 }
